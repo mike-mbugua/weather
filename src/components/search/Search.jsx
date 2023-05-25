216426ca44b3130c0cc7c6dce385b99b19962d9b
michael-mbugua <kamirimichael369@gmail.com>
2023-05-25 11:54:37 +0300

    return the data from the api using load options

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API, geoApiOptions } from "../api";
 
 function Search({ onSearchChange }) {
   const [search, setSearch] = useState(null);
   const handleChange = (searchData) => {
     setSearch(searchData);
     onSearchChange(searchData);
+  };
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((res) => {
+        return {
+          options: res.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name},${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <>
@@ -14,6 +34,7 @@ function Search({ onSearchChange }) {
         debounceTimeout={600}
         value={search}
         onChange={handleChange}
+        loadOptions={loadOptions}
       />
     </>
   );
