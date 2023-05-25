f3f474bbec06ba5729bcdfdc1410262e991dab02
michael-mbugua <kamirimichael369@gmail.com>
2023-05-25 11:54:09 +0300

    create an api component to import it later

---
+++ src/components/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b6ba66f0c3msh362c19c44f91423p11a856jsnffcadbf11860",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API = "https://wft-geo-db.p.rapidapi.com/v1/geo";
